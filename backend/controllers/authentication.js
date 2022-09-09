12d5ac8e6fa69d031485072a49d0115181f2b7dc
Mal F <malfellows13@gmail.com>
2022-09-09 18:40:56 -0500

    user can be found by email

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -8,7 +8,16 @@ const bcrypt = require('bcrypt');
 const { User } = db
 
 router.post('/', async (req, res) => {
-    console.log('IN HERE')
+    // initial check to see if login goes through
+    // console.log('IN HERE')
+
+    // checks credentials that the user provides and compares it to data in database
+    // uses user model to find the user with whatever email the person entered
+    let user = await User.findOne({
+        where: { email: req.body.email }
+    })
+
+    console.log(user)
 })
 
 // EXPORT
