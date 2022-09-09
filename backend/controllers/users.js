e4f4c6cd7535eb721672e81d2789fbaf37ad34a9
Mal F <malfellows13@gmail.com>
2022-09-09 18:01:47 -0500

    password hashing works with bcrypt

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,10 +1,20 @@
 const router = require('express').Router()
 const db = require("../models")
+// require bcrypt
+const bcrypt = require('bcrypt')
 
 const { User } = db
 
+// runs when sign-up form is submitted and where we'll want to hash the user's password
+// need to run "npm install bcrypt" in back-end directly terminal first
 router.post('/', async (req, res) => {
-    const user = await User.create(req.body)
+    // variable for password
+    let { password, ...rest } = req.body;
+    // hashes password
+    const user = await User.create({
+        ...rest,
+        passwordDigest: await bcrypt.hash(password, 10)
+    })
     res.json(user)
 })
 
