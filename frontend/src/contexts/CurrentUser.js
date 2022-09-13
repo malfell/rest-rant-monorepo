32f528411587295adcb26d48b9b47864b01e8bda
Mal F <malfellows13@gmail.com>
2022-09-13 00:17:37 -0500

    jwt included in fetch requests

--- frontend/src/contexts/CurrentUser.js
+++ frontend/src/contexts/CurrentUser.js
@@ -9,7 +9,15 @@ function CurrentUserProvider({ children }){
     // sends a fetch request
     useEffect(() => {
         const getLoggedInUser = async () => {
-            let response = await fetch('http://localhost:5000/authentication/profile')
+            let response = await fetch('http://localhost:5000/authentication/profile', {
+                // attaches JWT to fetch request, so backend will look at that to find
+                // the user who is logged in
+                // "Bearer" is not really required for authentication to work,
+                // but it's conventional when sending authorization tokens to API
+                headers: {
+                    'Authorization': `Bearer ${localStorage.getItem('token')}`
+                }
+            })
             let user = await response.json()
             setCurrentUser(user)
         }
